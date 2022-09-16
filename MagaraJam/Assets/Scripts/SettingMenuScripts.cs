ed8aa092f5fde30cfed32f0b770244e4d0b0c157
SeckinKurtalan <seckinkurtalan02@gmail.com>
2022-09-16 15:12:42 +0300

    Start Menu Completed

--- MagaraJam/Assets/Scripts/SettingMenuScripts.cs
+++ MagaraJam/Assets/Scripts/SettingMenuScripts.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
 
 public class SettingMenuScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] AudioMixer mainMixer;
+    
+    public void SetFullScreen(bool isFullscreen)
     {
-        
+        Screen.fullScreen = isFullscreen;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetQuality(int qualityIndex)
     {
-        
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+
+    public void StartTheGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
 }
