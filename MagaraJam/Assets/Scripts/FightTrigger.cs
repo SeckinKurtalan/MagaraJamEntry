f51efe9607e98a873f32f448696ebe3c012966a6
eskin04 <102257443+gdscgame@users.noreply.github.com>
2022-09-19 19:29:20 +0300

    gerçekten son commit

--- MagaraJam/Assets/Scripts/FightTrigger.cs
+++ MagaraJam/Assets/Scripts/FightTrigger.cs
@@ -8,9 +8,11 @@ public class FightTrigger : MonoBehaviour
     [SerializeField] AudioSource backgroundMusic;
     [SerializeField] GameObject[] roads;
     [SerializeField] GameObject enemieTag;
+    [SerializeField] GameObject fightText;
     bool isInFightArea;
     bool isOutFightArea;
     string enemieTagString;
+
     private void Start()
     {
         enemieTagString = enemieTag.tag;
@@ -20,6 +22,7 @@ public class FightTrigger : MonoBehaviour
         if (other.gameObject.tag == "Player" && !isInFightArea)
         {
             fightArea.SetActive(true);
+            fightText.SetActive(true);
             backgroundMusic.Stop();
             isInFightArea = true;
             foreach (GameObject road in roads)
@@ -39,6 +42,7 @@ public class FightTrigger : MonoBehaviour
                     isOutFightArea = true;
                     fightArea.SetActive(false);
                     backgroundMusic.Play();
+                    fightText.SetActive(false);
                     foreach (GameObject road in roads)
                     {
                         road.SetActive(true);
